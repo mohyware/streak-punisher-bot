facfb384e6801975ddcced4b1a76eed832bd29dc
mohyware <mohieelden50@gamil.com>
2025-01-10 23:28:38 +0200

    feat: reorganize database scripts

---
+++ services/database/scripts/deleteDb-script.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const User = require('../../../models/user-model');
+const Problem = require('../../../models/problem-model');
+
+// Replace with your MongoDB URI
+const mongoURI = process.env.MONGO_URI;
+
+// Connect to MongoDB
+mongoose.connect(mongoURI, {})
+    .then(async () => {
+        console.log('Connected to MongoDB');
+
+        try {
+            // Fetch all documents from the 'User' collection
+            const users = await User.deleteMany();
+            console.log('Users:', users);
+
+            // Fetch all documents from the 'Problem' collection
+            const problems = await Problem.deleteMany();
+            console.log('Problems:', problems);
+        } catch (err) {
+            console.error('Error fetching documents:', err);
+        } finally {
+            // Close the connection
+            mongoose.connection.close();
+        }
+    })
+    .catch(err => {
+        console.error('MongoDB connection error:', err);
+    });
